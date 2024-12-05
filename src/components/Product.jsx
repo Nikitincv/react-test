bc980df1b4d1f4b011f5cfe6bcbb292ce77df70c
Nikitin <161200278+Nikitincv@users.noreply.github.com>
2024-12-05 17:16:02 +0100

    src

---
+++ src/components/Product.jsx
@@ -0,0 +1,57 @@
+
+
+// export default function Product() {
+//       const price = 997;
+//       const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"; 
+
+
+//       return (
+//       <div>
+//       <h2>Tacos</h2>
+// 	  <img src= {imgUrl} alt="Tacos With Lime" width="640" />
+// 	  <p>Price: {price} credits</p>
+//     </div>
+//       );
+//     };
+    // src/components/App.jsx
+
+//     export default function Product({
+//       name,
+//       imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+//        price
+//       }) {
+//       return (
+//         <div>
+//           <h2>{name}</h2>
+//             <img src= {imgUrl} alt={name} width="480" />
+//             <p>Price: {price} credits</p>
+//         </div>
+//       );
+//     };
+
+
+
+// src/components/Product.jsx
+
+import PropTypes from 'prop-types';
+
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+		<img src={imgUrl} alt={name} width="480" />
+		<h2>{name}</h2>
+    <p>Price: {price} credits</p>
+  </div>
+);
+
+// Опис типів пропсів компонента 
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
+};
+
+export default Product;
